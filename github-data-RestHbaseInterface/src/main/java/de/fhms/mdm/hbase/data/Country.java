60981063466dcd19e8680775321576bc7a953ddb
Marcel Grawenhoff <marcel.grawenhoff@gmail.com>
2016-02-21 11:07:55 +0100

    presentation der weltkarte

---
+++ github-data-RestHbaseInterface/src/main/java/de/fhms/mdm/hbase/data/Country.java
@@ -0,0 +1,48 @@
+package de.fhms.mdm.hbase.data;
+
+public class Country {
+
+	
+	private String country;
+
+	private double longitude;
+	private double latitude;
+	private long amount;
+	
+
+	public Country(String country, double longitude, double latitude, long amount) {
+		super();
+		this.country = country;
+		this.longitude = longitude;
+		this.latitude = latitude;
+		this.amount = amount;
+	}
+	public double getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+	public double getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public long getAmount() {
+		return amount;
+	}
+	public void setAmount(long amount) {
+		this.amount = amount;
+	}
+	
+	
+	
+	
+}
---
+++ github-data-RestHbaseInterface/src/main/java/de/fhms/mdm/hbase/data/Repo.java
@@ -0,0 +1,19 @@
+package de.fhms.mdm.hbase.data;
+
+public class Repo {
+	private String repo;
+
+	public String getRepo() {
+		return repo;
+	}
+
+	public void setRepo(String repo) {
+		this.repo = repo;
+	}
+
+	public Repo(String repo) {
+		super();
+		this.repo = repo;
+	}
+
+}
